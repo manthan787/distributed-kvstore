551420c5e4c7e326bb8bfecc707ff6bc40cc4ddf
Manthan Thakar <manthant15@gmail.com>
2017-11-17 20:17:08 -0500

    Implement abstract methods in InMemoryStore

--- server/store.py
+++ server/store.py
@@ -23,6 +23,13 @@ class InMemoryStore(Store):
 		return self.db[key]
 
 	def put(self, key, value):
+		if key in db: return False
 		self.db[key] = value
+		return True
 
-	def 
+	def batch_put(self, kvs):
+		stats = {'keys_added': 0, 'keys_failed': 0}
+		for kv in kvs:
+			if self.put(kv["key"], kv["value"]): stats[keys_added] += 1
+			else: stats[keys_failed] += 1
+		return stats
