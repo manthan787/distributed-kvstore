c998f8b148cc3a5ef8e7ab540d8baf06f7b26fba
Manthan Thakar <manthant15@gmail.com>
2017-11-18 17:19:08 -0500

    Indent using spaces, update interface

--- server/store.py
+++ server/store.py
@@ -1,35 +1,58 @@
 from abc import ABCMeta, abstractmethod
 
-
 class Store(object):
-	__metaclass__ = ABCMeta
+    __metaclass__ = ABCMeta
 
-	@abstractmethod
-	def get(self, key): pass
+    @abstractmethod
+    def get_all(self): pass
 
-	@abstractmethod
-	def put(self, key, value): pass
+    @abstractmethod
+    def get(self, key): pass
 
-	@abstractmethod
-	def batch_put(self, kvs): pass
+    @abstractmethod
+    def put(self, key, value): pass
+
+    @abstractmethod
+    def batch_put(self, kvs): pass
 
 
 class InMemoryStore(Store):
 
-	def __init__(self):
-		self.db = {}
+    def __init__(self):
+        self.encodings = {}
+        self.data = {}
 
-	def get(self, key):
-		return self.db[key]
+    def get_all(self):
+        for k in self.data:
+            data, success = self.get({"data": k})
+            if success: yield data
 
-	def put(self, key, value):
-		if key in db: return False
-		self.db[key] = value
-		return True
+    def get(self, key):
+        try:
+            key_data = key['data']
+            return {"key": self._get_payload(key_data), \
+                    "value": self._get_payload(self.data[key_data])}, True
+        except Exception as e:
+            print e
+            return {}, False
 
-	def batch_put(self, kvs):
-		stats = {'keys_added': 0, 'keys_failed': 0}
-		for kv in kvs:
-			if self.put(kv["key"], kv["value"]): stats[keys_added] += 1
-			else: stats[keys_failed] += 1
-		return stats
+    def put(self, key, value):
+        try:
+            if key['data'] in self.data: return False
+            self.encodings[key['data']] = key['encoding']
+            self.encodings[value['data']] = value['encoding']
+            self.data[key['data']] = value['data']
+        except KeyError as e:
+            print e
+            return False
+        return True
+
+    def batch_put(self, kvs):
+        stats = {'keys_added': 0, 'keys_failed': 0}
+        for kv in kvs:
+            if self.put(kv["key"], kv["value"]): stats[keys_added] += 1
+            else: stats[keys_failed] += 1
+        return stats
+
+    def _get_payload(self, k):
+        return {"encoding": self.encodings[k], "data": k}
