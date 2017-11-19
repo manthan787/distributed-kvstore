5dbe260124288748a50949f3991f702843bb8b35
tirthraj <tirthraj@ccs.neu.edu>
2017-11-19 01:30:38 -0500

    modularized setHandler

--- proxy.go
+++ proxy.go
@@ -30,16 +30,12 @@ type Response struct {
 }
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", rootHandler)
-    router.HandleFunc("/set", setHandler).Methods("PUT")
-    router.HandleFunc("/fetch", fetchHandler).Methods("GET", "POST")
-    router.HandleFunc("/query", queryHandler).Methods("POST")
-    log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func servers() []string {
-	return os.Args[1:]
+  router := mux.NewRouter().StrictSlash(true)
+  router.HandleFunc("/", rootHandler)
+  router.HandleFunc("/set", setHandler).Methods("PUT")
+  router.HandleFunc("/fetch", fetchHandler).Methods("GET", "POST")
+  router.HandleFunc("/query", queryHandler).Methods("POST")
+  log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,36 +43,58 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func setHandler(w http.ResponseWriter, r *http.Request) {
-	s := servers()
-	elements := make([]Element, 0)
-	json.NewDecoder(r.Body).Decode(&elements)
-	requests := createSetRequests(elements)
 	var aggRes Response
-
+	s := servers()
+	requests := createSetRequests(decodedReq(r))
 	for i, req := range(requests) {
 		if len(req) > 0 {
-			url := "http://" + s[i] + "/set"
-			reqBody := new(bytes.Buffer)
-			json.NewEncoder(reqBody).Encode(req)
-
-			res, _ := resty.R().
-	      SetHeader("Content-Type", "application/json").
-	      SetBody(reqBody).
-	      Put(url)
-
 			var response Response
+			reqBody := createReqBody(req)
+			res := put("http://" + s[i] + "/set", reqBody)
 			json.Unmarshal(res.Body(), &response)
-			mergeRes(&aggRes, response)
+			mergeRes(&aggRes, &response)
 		}
 	}
-
-
 	setStatusCode(w, &aggRes)
-
 	json.NewEncoder(w).Encode(aggRes)
 }
 
-func mergeRes(aggRes *Response, res Response) {
+func servers() []string {
+	return os.Args[1:]
+}
+
+func decodedReq(r *http.Request) []Element {
+	elements := make([]Element, 0)
+	json.NewDecoder(r.Body).Decode(&elements)
+	return elements
+}
+
+func createSetRequests(elements []Element) [][]Element {
+	numOfServers := len(servers())
+	serverKeys := initServerKVs(numOfServers)
+	for _, kv := range(elements) {
+		serverIndex := hash(kv.Key.Data) % numOfServers
+		encode(&kv)
+		serverKeys[serverIndex] = append(serverKeys[serverIndex], kv)
+	}
+	return serverKeys
+}
+
+func createReqBody(req []Element) *bytes.Buffer {
+	reqBody := new(bytes.Buffer)
+	json.NewEncoder(reqBody).Encode(req)
+	return reqBody
+}
+
+func put(url string, reqBody *bytes.Buffer) *resty.Response {
+	res, _ := resty.R().
+	  SetHeader("Content-Type", "application/json").
+		SetBody(reqBody).
+		Put(url)
+	return res
+}
+
+func mergeRes(aggRes *Response, res *Response) {
 	aggRes.KeysAdded += res.KeysAdded
 	aggRes.KeysFailed = append(aggRes.KeysFailed, res.KeysFailed...)
 }
@@ -89,25 +107,6 @@ func setStatusCode(w http.ResponseWriter, aggRes *Response) {
 	}
 }
 
-func createSetRequests(elements []Element) [][]Element {
-	numOfServers := len(servers())
-	serverKeys := initServerKVs(numOfServers)
-	for _, kv := range(elements) {
-		serverIndex := hash(kv.Key.Data) % numOfServers
-
-		if kv.Key.Encoding == "binary" {
-			kv.Key.Data = base64.StdEncoding.EncodeToString([]byte(kv.Key.Data))
-		}
-
-		if kv.Value.Encoding == "binary" {
-			kv.Value.Data = base64.StdEncoding.EncodeToString([]byte(kv.Value.Data))
-		}
-
-		serverKeys[serverIndex] = append(serverKeys[serverIndex], kv)
-	}
-	return serverKeys
-}
-
 func initServerKVs(numOfServers int) [][]Element {
 	serverKeys := make([][]Element, numOfServers)
 	for i:=0; i < numOfServers; i++ {
@@ -116,10 +115,17 @@ func initServerKVs(numOfServers int) [][]Element {
 	return serverKeys
 }
 
-// func encode()
+func encode(kv *Element) {
+	if kv.Key.Encoding == "binary" {
+			kv.Key.Data = base64.StdEncoding.EncodeToString([]byte(kv.Key.Data))
+	}
+	if kv.Value.Encoding == "binary" {
+		kv.Value.Data = base64.StdEncoding.EncodeToString([]byte(kv.Value.Data))
+	}
+}
 
 func hash(s string) int {
-        h := fnv.New32a()
-        h.Write([]byte(s))
-        return int(h.Sum32())
+  h := fnv.New32a()
+  h.Write([]byte(s))
+  return int(h.Sum32())
 }
