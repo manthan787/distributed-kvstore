928cfcc7bb987c4989d51ad6c97c7d9686947ad1
Manthan Thakar <manthant15@gmail.com>
2017-11-19 00:12:48 -0500

    Handle GET requests on /fetch

--- handler.go
+++ handler.go
@@ -4,10 +4,44 @@ import (
     "fmt"
     "html"
     "net/http"
+    "log"
+    // "io/ioutil"
+    "encoding/json"
 )
 
 func fetchHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	switch r.Method {
+		case "GET":
+			fetchGetHandler(w, r)
+		case "POST":
+			fetchPostHandler(w, r)
+		default:
+			log.Println("Invalid Method for /fetch")
+	}
+}
+
+func fetchGetHandler(w http.ResponseWriter, r *http.Request) {
+	var allElements []Element
+	serverAddrs := servers()
+	for _, s := range serverAddrs {
+		allElements = append(allElements, fetchFromServer(s)...)
+	}
+    json.NewEncoder(w).Encode(allElements)
+}
+
+func fetchFromServer(server string) []Element {
+	log.Println("Querying Server ", server)
+	elements := make([]Element, 0)
+	resp, err := http.Get(server + "/fetch")
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+	json.NewDecoder(resp.Body).Decode(&elements)
+	return elements
+}
+
+func fetchPostHandler(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func queryHandler(w http.ResponseWriter, r *http.Request) {
